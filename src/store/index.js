016d86980086759e1370291a46e638b6ce2c0e4e
Dylan <1447240529@qq.com>
2025-03-27 17:35:16 +0800

    feat(store): 优化 Store 配置，添加系统模块持久化

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { createPinia } from "pinia";
 import { createPersistedState } from "pinia-plugin-persistedstate";
+import { useUserStore } from "./modules/user";
+import { useAccountStore } from "./modules/account";
+import { useSystemStore } from "./modules/system";
 
 const pinia = createPinia();
 
@@ -8,8 +11,14 @@ pinia.use(
   createPersistedState({
     key: (id) => `companion_${id}`,
     storage: localStorage,
-    paths: ["user.token", "user.userInfo"],
+    paths: [
+      "user.token",
+      "user.userInfo",
+      "system.systemTags",
+      "system.userTags",
+    ],
   })
 );
 
+export { useUserStore, useAccountStore, useSystemStore };
 export default pinia;
